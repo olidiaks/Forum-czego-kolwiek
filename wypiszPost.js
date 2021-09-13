8a12c23defaf21bb840facdb63656c934c2366a5
olidiaks <olidiaks@nie.podam>
2021-09-13 14:21:50 +0200

    Uokńczenie intreface.

--- wypiszPost.js
+++ wypiszPost.js
@@ -1,15 +1,16 @@
-import { dzienMiesiacRok, godzinaMinutaSekunda } from "./formatowanieCzasu.js";
+import { dzienMiesiacRok, godzinaMinutaSekunda } from './formatowanieCzasu.js';
 
 export const wypiszPost = () => {
-    
-    const nazwaUzytkownika = document.getElementById('inputNazwaUżytkownika').value;
-    const trescWpisu = document.getElementById('textareaWpis').value;
-    const sekcjaWpisow = document.getElementById(`wpis`);
-    const post = `
-        <div class="post">
-            <p  class="nazwaUzytkownika">Post został opublikowany dnia ${dzienMiesiacRok()} o godzinie ${godzinaMinutaSekunda()}, przez użytkownika: ${nazwaUzytkownika}</p>
-            <p class="wpisUrzytkownika">${trescWpisu}</p>
+	const nazwaUzytkownika = document.getElementById(
+		'inputNazwaUżytkownika',
+	).value;
+	const trescWpisu = document.getElementById('textareaWpis').value;
+	const sekcjaWpisow = document.getElementById(`wpis`);
+	const post = `
+        <div class="post zaokronglonaBialaObramowka3px">
+            <p  class="nazwaUzytkownika zaokronglonaBialaObramowka3px">Post został opublikowany dnia ${dzienMiesiacRok()} o godzinie ${godzinaMinutaSekunda()}, przez użytkownika: ${nazwaUzytkownika}</p>
+            <p class="wpisUrzytkownika zaokronglonaBialaObramowka3px">${trescWpisu}</p>
         </div>
     `;
-    sekcjaWpisow.innerHTML += post;
+	sekcjaWpisow.innerHTML += post;
 };
