430611d2ee9f9b5fb508768e764c65ef870dc70d
olidiaks <olidiaks@nie.podam>
2021-09-13 13:28:09 +0200

    Problem z wywoływaniem kilka razy dsico został rozwiązany.

--- discoMode.js
+++ discoMode.js
@@ -2,8 +2,11 @@ import { czyDiscoZostaloNapisane } from './czyDiscoZostaloWpisane.js';
 import { koloryCzerwony } from './tablicaKoloryRGB.js';
 import { zmienKolorTla } from './zmianaKoloruTla.js';
 
+let czyDiscoZostaloWywolane = false;
+
 export const discoMode = (key) => {
-	if (czyDiscoZostaloNapisane(key)) {
+	if (czyDiscoZostaloNapisane(key) && !czyDiscoZostaloWywolane) {
+		czyDiscoZostaloWywolane = true;
 		let i = 0;
 		setInterval(() => {
 			i++;
